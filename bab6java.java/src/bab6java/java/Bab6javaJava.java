6e057af8acbb95091abe9ee3263003252b12e146
twety022 <deaaqilasoleha@gmail.com>
2025-08-06 12:29:06 +0700

    Add files via upload

---
+++ bab6java.java/src/bab6java/java/Bab6javaJava.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab6java.java;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PC 27 - LAB R1
+ */
+public class Bab6javaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        String temp = JOptionPane.showInputDialog(null, 
+//                "Masukan Angka", 
+//                "Menentukan Ganjil Genap", 
+//                JOptionPane.QUESTION_MESSAGE);
+//
+//        // ubah input dari string ke integer
+//        int input = Integer.parseInt(temp);
+//
+//        // tampilkan hasil di console
+//        System.out.println("Input: " + input);
+//
+//        // pengecekan bilangan genap atau ganjil
+//        if ((input % 2) == 0) {
+//            JOptionPane.showMessageDialog(null, 
+//                    "Angka: " + input + " adalah bilangan GENAP", 
+//                    "Informasi", 
+//                    JOptionPane.INFORMATION_MESSAGE);
+//        } else {
+//            JOptionPane.showMessageDialog(null, 
+//                    "Angka: " + input + " adalah bilangan GANJIL", 
+//                    "Informasi", 
+//                    JOptionPane.INFORMATION_MESSAGE);
+//        }
+
+//String umur = JOptionPane.showInputDialog(null, "Kamu umur berapa ?");
+//if (Integer.parseInt(umur) <= 5) {
+//JOptionPane.showMessageDialog(null, "Balita");
+//} else if (Integer.parseInt(umur) >= 6 && Integer.parseInt(umur) <= 11)
+//{ JOptionPane.showMessageDialog(null, "Anak - anak");
+//} else if (Integer.parseInt(umur) >= 12 && Integer.parseInt(umur) <= 25)
+//{ JOptionPane.showMessageDialog(null, "Remaja");
+//} else {
+//JOptionPane.showMessageDialog(null, "Dewasa");
+//}
+
+//String nilai = JOptionPane.showInputDialog
+//(null, "Masukan nilai :");
+//switch (nilai.toUpperCase()) {
+//case "A":
+//JOptionPane.showMessageDialog(null, "Sangat baik.");
+//break;
+//case "B":
+//JOptionPane.showMessageDialog(null, "Baik.");
+//break;
+//case "C":
+//JOptionPane.showMessageDialog(null, "Cukup.");
+//case "D":
+//JOptionPane.showMessageDialog(null, "Jelek.");
+//break;
+//default :
+//JOptionPane.showMessageDialog(null,
+//"Salah memasukan nilai !");
+//}
+
+
+    }
+}
