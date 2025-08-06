2a8cf18a6fd49daecabfeeff54c8f4a6423ea310
twety022 <deaaqilasoleha@gmail.com>
2025-08-06 12:29:25 +0700

    Add files via upload

---
+++ bab7java.java/src/bab7java/java/Bab7javaJava.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab7java.java;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PC 27 - LAB R1
+ */
+public class Bab7javaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//   String nama[] = new String[3];
+//nama[0] = "Annisa";
+//nama[1] = "Dea";
+//nama[2] = "Daffa";
+//
+//System.out.println("Index 0 : "+nama[0]);
+//System.out.println("Index 1 : "+nama[1]);
+//System.out.println("Index 2 : "+nama[2]);
+
+//String[] nama = new String[3];
+//
+//        // input dari user untuk masing-masing index
+//        nama[0] = JOptionPane.showInputDialog(null, "Nama :", "index ke-0", JOptionPane.INFORMATION_MESSAGE);
+//        nama[1] = JOptionPane.showInputDialog(null, "Nama :", "index ke-1", JOptionPane.INFORMATION_MESSAGE);
+//        nama[2] = JOptionPane.showInputDialog(null, "Nama :", "index ke-2", JOptionPane.INFORMATION_MESSAGE);
+//
+//        // menampilkan semua nama
+//        JOptionPane.showMessageDialog(null,
+//            nama[0] + ", " + nama[1] + ", " + nama[2],
+//            "Hasil Input Nama",
+//            JOptionPane.INFORMATION_MESSAGE);
+//  String umur[][] = new String[2][2];
+//umur[0][0] = "aqila";
+//umur[0][1] = "16";
+//umur[1][0] = "adsh";
+//umur[1][1] = "45";
+//
+//System.out.println("Nama : "+umur[0][0]+" umur : "+umur[0][1]);
+//System.out.println("Nama : "+umur[1][0]+" umur : "+umur[1][1]);
+  
+    }
+    
+}
